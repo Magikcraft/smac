d2bb421c7d836daec85afaf74c317f4d2470292a
Josh Wulf <josh@magikcraft.io>
2019-03-28 15:24:11 +1000

    npm run dev task

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp')
+const ts = require('gulp-typescript')
+const tsProject = ts.createProject('tsconfig.json')
+const rename = require('gulp-rename')
+gulp.task('typescript', function() {
+    console.log('Typescript')
+    return gulp
+        .src(['**/*.ts', '!node_modules/**']) // or tsProject.src()
+        .pipe(tsProject())
+        .pipe(gulp.dest('.'))
+})
+
+gulp.task('copy', function() {
+    return gulp
+        .src('./bin/smac.js')
+        .pipe(rename('smac'))
+        .pipe(gulp.dest('./bin/', { mode: 0755 }))
+})
+
+gulp.task('build', () => {
+    console.log('Hi!')
+    gulp.series(['typescript', 'copy'])
+})
+
+gulp.task('watch', () => {
+    gulp.watch('**/*.ts', gulp.series(['typescript', 'copy']))
+})
